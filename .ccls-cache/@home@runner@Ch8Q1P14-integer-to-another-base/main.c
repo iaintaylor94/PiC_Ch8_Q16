ed9c509a3f5fee54b0a7f4c14ddb79d120d111fb
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-24 13:28:29 +0000

    copied program

--- .ccls-cache/@home@runner@Ch8Q1P14-integer-to-another-base/main.c
+++ .ccls-cache/@home@runner@Ch8Q1P14-integer-to-another-base/main.c
@@ -1,6 +1,69 @@
+// Program to convert a positive integer to another base
+
 #include <stdio.h>
 
+// Declare Functions
+void getNumberAndBase (void);
+void convertNumber (void);
+void displayConvertedNumber (void);
+
+// Declare Global variables
+int gConvertedNumber [64];
+long int gNumberToConvert;
+int gBase;
+int gDigit = 0;
+
+
+
 int main(void) {
-  printf("Hello World\n");
+
+  getNumberAndBase();
+  convertNumber ();
+  displayConvertedNumber ();
+  
   return 0;
+}
+
+
+
+// Define Functions
+void getNumberAndBase (void) {
+  // Get number
+  printf ("Number to convert? ");
+  scanf ("%ld", &gNumberToConvert);
+
+  // Get base
+  printf ("Base? ");
+  scanf ("%d", &gBase);
+
+  // Check base is between 2 and 16
+  if (gBase < 2 || gBase > 16) {
+    printf ("Bad base - must be between 2 and 16\n"); 
+    gBase = 10;
+  }
+}
+
+void convertNumber (void) {
+  do {
+    gConvertedNumber [gDigit] = gNumberToConvert % gBase;
+    gDigit++;
+    gNumberToConvert /= gBase;
+  } 
+  while (gNumberToConvert != 0);
+}
+
+void displayConvertedNumber (void) {
+  const char baseDigits [16] = {
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+  };
+
+  int nextDigit;
+
+  // Print converted number
+  printf ("Converted number = ");
+  for (--gDigit; gDigit >= 0; gDigit--) {
+    nextDigit = gConvertedNumber[gDigit];
+    printf ("%c", baseDigits[nextDigit]);
+  }
+  printf ("\n");
 }
